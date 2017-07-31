a0102246c1913abd951b5c083bc35bc709a54488
Mufeez Ahmed <v-mufahm@microsoft.com>
2017-07-31 18:22:41 +0530

    removed extra info from parent describe in compose test

--- Test/Spec/officeComposeAPITest .js
+++ Test/Spec/officeComposeAPITest .js
@@ -79,7 +79,7 @@ function sendTestReport() {
 
 }
 
-describe("Compose: office api Test",
+describe("",
     function () {
         beforeAll(function (done) { setTimeout(function () { done(); }, 2000) });
         afterAll(function () {
