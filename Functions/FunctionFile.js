7bb2b8382f8b4705a83bb28e37471fa6a2f9cce4
Mufeez Ahmed <v-mufahm@microsoft.com>
2017-08-10 17:03:12 +0530

    Isolated Dispaly Dialogs in Function File ,added action in manifest  to execute functions inside function file

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -14,3 +14,117 @@ function statusUpdate(icon, text) {
 function defaultStatus(event) {
   statusUpdate("icon16" , "Hello World!");
 }
+
+
+function displayReplyForm(event) {
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* Display a reply form */
+    Office.context.mailbox.item.displayReplyForm(
+        {
+            'htmlBody': 'hi',
+            'attachments': [
+                {
+                    'type': Office.MailboxEnums.AttachmentType.File,
+                    'name': 'squirrel.png',
+                    'url': 'http://i.imgur.com/sRgTlGR.jpg'
+                },
+                {
+                    'type': Office.MailboxEnums.AttachmentType.Item,
+                    'name': 'mymail',
+                    'itemId': Office.context.mailbox.item.itemId
+                }
+            ]
+        }
+    );
+
+    event.completed();
+}
+
+function displayReplyAllForm(event) {
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* Display a reply all form */
+    Office.context.mailbox.item.displayReplyAllForm("hi");
+    event.completed();
+}
+
+function displayReplyFormWithInline() {
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* inline image - display reply form */
+    Office.context.mailbox.item.displayReplyForm(
+        {
+            'htmlBody': '<img src = "cid:squirrel.png">',
+            'attachments':
+            [
+                {
+                    'type': Office.MailboxEnums.AttachmentType.File,
+                    'name': 'squirrel.png',
+                    'url': 'http://i.imgur.com/sRgTlGR.jpg',
+                    'isInline': 'true'
+                }
+            ]
+        });
+    event.completed();
+}
+
+function displayReplyAllFormWithInline() {
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* inline image - display reply form */
+    Office.context.mailbox.item.displayReplyAllForm(
+        {
+            'htmlBody': '<img src = "cid:squirrel.png">',
+            'attachments':
+            [
+                {
+                    'type': Office.MailboxEnums.AttachmentType.File,
+                    'name': 'squirrel.png',
+                    'url': 'http://i.imgur.com/sRgTlGR.jpg',
+                    'isInline': 'true'
+                }
+            ]
+        });
+    event.completed();
+}
+
+function displayNewAppointmentForm(event)
+{
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* Display new appointment form */
+    var start = new Date();
+    var end = new Date();
+    end.setHours(start.getHours() + 1);
+
+    Office.context.mailbox.displayNewAppointmentForm(
+        {
+            requiredAttendees: ["bob@contoso.com"],
+            optionalAttendees: ["sam@contoso.com"],
+            start: start,
+            end: end,
+            location: "Home",
+            resources: ["projector@contoso.com"],
+            subject: "meeting",
+            body: "Hello World!"
+        });
+
+    event.completed();
+}
+
+function displayMessageForm(event)
+{
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* Display message form */
+    // Item ID of current message
+    var messageId = "AAMkAGZiZjc1Y2RkLTczNjktNGU1YS1hYTkzLTYzZTU3OTE5OWQ3NABGAAAAAAC3Bc26XexrR4XknrAwz6j9BwBDfaKHIE1iQJlAjLUe7EC6AACETbQlAABDfaKHIE1iQJlAjLUe7EC6AACI3qQ2AAA=";
+    Office.context.mailbox.displayMessageForm(messageId);
+    event.completed();
+}
+
+function displayAppointmentForm(event)
+{
+    /* ReadItem or ReadWriteItem or ReadWriteMailbox */
+    /* Display appointment form */
+    // Item ID of current appointment
+    var appointmentId = "AAMkAGZiZjc1Y2RkLTczNjktNGU1YS1hYTkzLTYzZTU3OTE5OWQ3NABGAAAAAAC3Bc26XexrR4XknrAwz6j9BwBDfaKHIE1iQJlAjLUe7EC6AAAAAAENAABDfaKHIE1iQJlAjLUe7EC6AABzLImxAAA=";
+    Office.context.mailbox.displayAppointmentForm(appointmentId);
+    event.completed();
+
+}
